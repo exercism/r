f2799f99bfe23107a49faf215f9b9e981af4bf59
Jonathan Boiser <jboiser@gmail.com>
2015-12-23 12:11:28 -0800

    Add hello-world problem to xr

---
+++ hello-world/tests/testthat/test_helloworld.R
@@ -0,0 +1,11 @@
+test_that("no name", {
+  expect_equal(hello_world(), "Hello, World!")
+})
+
+test_that("sample name", {
+  expect_equal(hello_world("Alice"), "Hello, Alice!")
+})
+
+test_that("other sample name", {
+  expect_equal(hello_world("Bob"), "Hello, Bob!")
+})
