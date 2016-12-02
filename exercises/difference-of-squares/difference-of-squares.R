40a02c67836ad479b13be15a4f48191cf99b9d72
Christian Miller <christian.david.miller@gmail.com>
2016-12-01 23:03:34 -0800

    add difference-of-squares exercise

---
+++ exercises/difference-of-squares/difference-of-squares.R
@@ -0,0 +1,5 @@
+# this is a stub function that takes a natural_number
+# and should return the difference-of-squares as described
+# in the README.md
+difference_of_squares <- function(natural_number) {
+}
