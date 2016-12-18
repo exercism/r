d08169ebe9d58fb8abeac40541c8980a3ca1b193
Christian Miller <christian.david.miller@gmail.com>
2016-12-18 14:04:34 -0800

    add working examples, update stub for luhn

--- exercises/anagram/example.R
+++ exercises/anagram/example.R
@@ -1,3 +1,19 @@
-anagram <- function() {
-
+anagram <- function(subject,candidates) {
+  # subject is a string
+  # candidates is a character vector
+  # break subject into vector of characters
+  subject <- tolower(subject)
+  test_candidates <- tolower(candidates)
+  l <- strsplit(subject,"")[[1]]
+  # sub out each character from l in candidates
+  for (i in l) {
+    test_candidates <- sub(i,"",test_candidates)
+  }
+  result <- candidates[test_candidates=="" &
+                         tolower(candidates) != subject &
+                         nchar(candidates) == length(l)]
+  if (length(result)==0) {
+    return(c())
+  }
+  return(result)
 }
