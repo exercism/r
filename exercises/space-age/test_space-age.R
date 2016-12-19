9ff5af452459636ba735ffdc9efaaf31320ab7fa
Jonathan Boiser <jonboiser@users.noreply.github.com>
2016-12-19 10:17:14 -0500

    change formatting of space age tests

--- exercises/space-age/test_space-age.R
+++ exercises/space-age/test_space-age.R
@@ -3,58 +3,42 @@ suppressPackageStartupMessages({ require(testthat) })
 
 test_that("Age on Earth", {
   seconds <- 1000000000
-  expect_equal(space_age(seconds,"earth"),
-    31.69
-  )
+  expect_equal(space_age(seconds, "earth"), 31.69)
 })
 
 test_that("Age on Mercury", {
   seconds <- 2134835688
-  expect_equal(space_age(seconds,"mercury"),
-    280.88
-  )
+  expect_equal(space_age(seconds, "mercury"), 280.88)
 })
 
 test_that("Age on Venus", {
   seconds <- 189839836
-  expect_equal(space_age(seconds,"venus"),
-    9.78
-  )
+  expect_equal(space_age(seconds, "venus"), 9.78)
 })
 
 test_that("Age on Mars", {
   seconds <- 2329871239
-  expect_equal(space_age(seconds,"mars"),
-    39.25
-  )
+  expect_equal(space_age(seconds, "mars"), 39.25)
 })
 
 test_that("Age on Jupiter", {
   seconds <- 901876382
-  expect_equal(space_age(seconds,"jupiter"),
-    2.41
-  )
+  expect_equal(space_age(seconds, "jupiter"), 2.41)
 })
 
 test_that("Age on Saturn", {
   seconds <- 3000000000
-  expect_equal(space_age(seconds,"saturn"),
-    3.23
-  )
+  expect_equal(space_age(seconds, "saturn"), 3.23)
 })
 
 test_that("Age on Uranus", {
   seconds <- 3210123456
-  expect_equal(space_age(seconds,"uranus"),
-    1.21
-  )
+  expect_equal(space_age(seconds,"uranus"), 1.21)
 })
 
 test_that("Age on Neptune", {
   seconds <- 8210123456
-  expect_equal(space_age(seconds,"neptune"),
-    1.58
-  )
+  expect_equal(space_age(seconds, "neptune"), 1.58)
 })
 
 print("All tests passed!")
