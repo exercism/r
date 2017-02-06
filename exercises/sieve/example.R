a0c4d2802afb008b878e36c69a44d79c576f9b9c
jonmcalder <jonmcalder@gmail.com>
2017-02-07 01:43:01 +0200

    Add sieve exercise

---
+++ exercises/sieve/example.R
@@ -0,0 +1,16 @@
+sieve <- function(limit) {
+  if (limit < 2) {
+    return(c())
+  }
+  nums <- rep(TRUE, limit)
+  nums[1] <- FALSE
+  for (i in 2:limit) {
+    if (nums[i]) {
+      div <- floor(limit/i)
+      if (div > 1){
+        nums[i*2:div] <- FALSE  
+      }
+    }
+  }
+  which(nums)
+}
---
+++ exercises/sieve/sieve.R
@@ -0,0 +1,3 @@
+sieve <- function(limit) {
+  
+}
