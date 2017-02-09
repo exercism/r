3a14a98cab25dc5b42f9f4f9e24b47c125aa1dac
jonmcalder <jonmcalder@gmail.com>
2017-02-09 21:44:03 +0200

    Add perfect-numbers exercise

---
+++ exercises/perfect-numbers/example.R
@@ -0,0 +1,22 @@
+isPerfect <- function(n){
+  
+  findFactors <- function(n) {
+    factors <- c()
+    for (i in 2:floor(n^0.5 + 1)) {
+      
+      if (n %% i == 0) {
+        if (i^2 != n) {
+          factors <- c(factors, i, n / i)
+        }
+        else {
+          factors <- c(factors, i)
+        }
+      }
+            
+    }
+    unique(factors)
+  }
+  
+  sum(findFactors(n)) + 1 == n
+  
+}
---
+++ exercises/perfect-numbers/perfect-numbers.R
@@ -0,0 +1,3 @@
+isPerfect <- function(n){
+  
+}
