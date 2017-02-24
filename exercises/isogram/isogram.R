b4b958a463746d646b37fbe0d731910f81d2e087
Jon Calder <jonmcalder@gmail.com>
2017-02-24 16:44:05 +0200

    Add isogram exercise

---
+++ exercises/isogram/isogram.R
@@ -0,0 +1,3 @@
+is_isogram <- function(word) {
+  
+}
---
+++ exercises/isogram/test_isogram.R
@@ -0,0 +1,44 @@
+source('./isogram.R')
+suppressPackageStartupMessages({ require(testthat) })
+
+test_that("empty string", {
+  word <- ""
+  expect_equal(is_isogram(word), TRUE)
+})
+
+test_that("isogram with only lower case characters", {
+  word <- "isogram"
+  expect_equal(is_isogram(word), TRUE)
+})
+
+test_that("word with one duplicated character", {
+  word <- "eleven"
+  expect_equal(is_isogram(word), FALSE)
+})
+
+test_that("longest reported english isogram", {
+  word <- "subdermatoglyphic"
+  expect_equal(is_isogram(word), TRUE)
+})
+
+test_that("word with duplicated character in mixed case", {
+  word <- "Alphabet"
+  expect_equal(is_isogram(word), FALSE)
+})
+
+test_that("hypothetical isogrammic word with hyphen", {
+  word <- "thumbscrew-japingly"
+  expect_equal(is_isogram(word), TRUE)
+})
+
+test_that("isogram with duplicated non letter character", {
+  word <- "Hjelmqvist-Gryb-Zock-Pfund-Wax"
+  expect_equal(is_isogram(word), TRUE)
+})
+
+test_that("made-up name that is an isogram", {
+  word <- "Emily Jung Schwartzkopf"
+  expect_equal(is_isogram(word), TRUE)
+})
+
+print("All tests passed!")
