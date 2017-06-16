00e2649e50ba895db2c93c276d7316cbd981710f
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-16 17:26:03 +0200

    update from x-common
    https://github.com/exercism/x-common/commit/39cba0d4f709a407a234c2cab327bb2e0f005c93
    
    [x] make example.R pass again

--- exercises/phone-number/example.R
+++ exercises/phone-number/example.R
@@ -1,7 +1,8 @@
 parse_phone_number <- function(number_string) {
   
   # If the number is less than 10 digits assume that it is bad number
-  # If the number is 10 digits assume that it is good
+  # If the number is 10 digits assume that it is good, unless area or exchange 
+  #  code are not between 2 to 9
   # If the number is 11 digits and the first number is 1, use the last 10 digits
   # If the number is 11 digits and the first number is not 1, it is a bad number
   # If the number is more than 11 digits assume that it is a bad number
@@ -15,6 +16,10 @@ parse_phone_number <- function(number_string) {
   cleaned <- gsub("[^0-9]", "", number_string)
   
   if (nchar(cleaned) < 10) {
+    NULL
+  }
+  else if (nchar(cleaned) == 10 & (substr(cleaned, 1, 1) %in% c("0", "1") |
+                                   substr(cleaned, 4, 4) %in% c("0", "1"))) {
     NULL
   } 
   else if (nchar(cleaned) == 10) {
--- exercises/phone-number/test_phone-number.R
+++ exercises/phone-number/test_phone-number.R
@@ -2,30 +2,30 @@ source("./phone-number.R")
 library(testthat)
 
 test_that("cleans the number", {
-  expect_equal(parse_phone_number("(123) 456-7890"), "1234567890")
+  expect_equal(parse_phone_number("(223) 456-7890"), "2234567890")
 })
 
 test_that("cleans numbers with dots", {
-  expect_equal(parse_phone_number("123.456.7890"), "1234567890")
+  expect_equal(parse_phone_number("223.456.7890"), "2234567890")
 })
 
 test_that("cleans numbers with multiple spaces", {
-  expect_equal(parse_phone_number("123 456   7890   "), "1234567890")
+  expect_equal(parse_phone_number("223 456   7890   "), "2234567890")
 })
 
 test_that("invalid when 9 digits", {
   expect_equal(parse_phone_number("123456789"), NULL)
 })
 
-test_that("invalid when 11 digits", {
-  expect_equal(parse_phone_number("21234567890"), NULL)
+test_that("invalid when 11 digits and not starting with a 1", {
+  expect_equal(parse_phone_number("22234567890"), NULL)
 })
 
 test_that("valid when 11 digits and starting with 1", {
-  expect_equal(parse_phone_number("11234567890"), "1234567890")
+  expect_equal(parse_phone_number("12234567890"), "2234567890")
 })
 
-test_that("invalid when 12 digits", {
+test_that("invalid when more than 11 digits", {
   expect_equal(parse_phone_number("321234567890"), NULL)
 })
 
@@ -37,8 +37,12 @@ test_that("invalid with punctuations", {
   expect_equal(parse_phone_number("123-@:!-7890"), NULL)
 })
 
-test_that("invalid with right number of digits but letters mixed in", {
-  expect_equal(parse_phone_number("1a2b3c4d5e6f7g8h9i0j"), NULL)
+test_that("invalid if area code does not start with 2-9", {
+  expect_equal(parse_phone_number("(123) 456-7890"), NULL)
+})
+
+test_that("invalid if exchange code does not start with 2-9", {
+  expect_equal(parse_phone_number("(223) 056-7890"), NULL)
 })
 
 print("All tests passed for exercise: phone-number")
