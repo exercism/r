dea0572e98cb3cf6681b886038f2b7b69b13867d
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-06 17:08:03 +0200

    fulfill all test with naive implementation
    leaves room for deduplication and optimising for readability, etc.

--- exercises/beer-song/example.R
+++ exercises/beer-song/example.R
@@ -1,43 +1,31 @@
-library(magrittr)
-
 lyrics <- function(first, last) {
+  # compile song from dynamic number of verses
+  song <- c()
+  for (i in seq(first, last)) {
+    song <- c(song, verse(i))
+  }
   
+  paste(song, collapse = "\n")
 }
 
 verse <- function(number) {
-  
-  clean_verse <- function(v)
-    unlist(gsub("\\ +", " ", v))
-  
-  # sequence of verses with same text
-  b <- "bottle"
-  ob <- "of beer"
-  otw <- "on the wall"
-  todapia <- "Take one down and pass it around"
-  
-  # different text for last two verses
-  om <- "o more"
-  tidapia <- "Take it down and pass it around"
-  gttsabsm <- "Go to the store and buy some more"
-  
-  # abbreviate variable
-  n <- number
-  
-  if (n == 2) {
-    sprintf("2 %ss %s %s, 2 %ss %s.\n%s, 1 %s %s %s.\n",
-            b, ob, otw, b, ob, todapia, b, ob, otw) %>% 
-      clean_verse
-  } else if (n == 1) {
-    sprintf("1 %s %s %s, 1 %s %s.\n%s, n%s %ss %s %s.\n",
-            b, ob, otw, b, ob,     tidapia, om, b, ob, otw) %>% 
-      clean_verse
-  } else if (n == 0) {
-    sprintf("N%s %ss %s  %s,  n%s %ss %s.\n%s, 99    %ss %s  %s.\n",
-            om, b,  ob, otw, om, b,  ob,  gttsabsm, b,  ob, otw) %>% 
-      clean_verse
+  # construct different verses
+  if (number == 2) {
+    paste(sep = "\n",
+      "2 bottles of beer on the wall, 2 bottles of beer.",
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n")
+  } else if (number == 1) {
+    paste(sep = "\n",
+      "1 bottle of beer on the wall, 1 bottle of beer.",
+      "Take it down and pass it around, no more bottles of beer on the wall.\n")
+  } else if (number == 0) {
+    paste(sep = "\n",
+      "No more bottles of beer on the wall, no more bottles of beer.",
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
   } else {
-    sprintf("%d %ss %s %s, %d %ss %s.%s%s, %d %ss %s %s.\n",
-            n, b, ob, otw, n, b, ob, "\n", todapia, n-1, b, ob, otw) %>% 
-      clean_verse
+    sprintf(paste(sep = "\n",
+        "%d bottles of beer on the wall, %d bottles of beer.",
+        "Take one down and pass it around, %d bottles of beer on the wall.\n"),
+      number, number, number - 1)
   }
 }
