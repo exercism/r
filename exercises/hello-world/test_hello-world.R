7fd13ec2c3487af6b3a3e05753eaca594166426e
TN <tatiana_nagata@hotmail.com>
2017-11-16 07:27:27 +0900

    hello-world: Fix tests to match README.md
    Remove tests that call hello-world with arguments.
    
    Closes #82

--- exercises/hello-world/test_hello-world.R
+++ exercises/hello-world/test_hello-world.R
@@ -5,12 +5,4 @@ test_that("no name", {
   expect_equal(hello_world(), "Hello, World!")
 })
 
-test_that("sample name", {
-  expect_equal(hello_world("Alice"), "Hello, Alice!")
-})
-
-test_that("other sample name", {
-  expect_equal(hello_world("Bob"), "Hello, Bob!")
-})
-
 message("All tests passed for exercise: hello-world")
