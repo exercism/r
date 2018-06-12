f4beddfb9f31159b9a298ecfda5de06ccf2305f4
Zacchaeus <zacsnape@gmail.com>
2018-06-12 13:33:59 +0100

    Fix linting & improve readability

--- exercises/acronym/example.R
+++ exercises/acronym/example.R
@@ -2,8 +2,11 @@ acronym <- function(input) {
   # split the string by spaces or hyphens
   split <- strsplit(input, " |-")
   
-  #get the first letter of each substring
+  # get the first letter of each substring
   first_letters <- substring(split[[1]], 1, 1)
+
+  # join the acronym back together & make uppercase
+  acronym <- paste(toupper(first_letters), collapse = "")
   
-  return(paste(toupper(first_letters), collapse=""))
+  return(acronym)
 }
