86871b06396f18d800431be63d9c2112793d68a7
Scientifica96 <bentbibrachad@gmail.com>
2018-08-30 18:08:56 +0100

    difference-of-squares: renamed square_sums to square_sum

--- exercises/difference-of-squares/example.R
+++ exercises/difference-of-squares/example.R
@@ -1,6 +1,6 @@
 difference_of_squares <- function(natural_number) {
     num_sequence <- seq_len(natural_number)
-    square_sums <- sum(num_sequence) ^ 2
+    square_sum <- sum(num_sequence) ^ 2
     sum_squares <- sum(num_sequence ^ 2)
-    sum(square_sums - sum_squares)
+    sum(square_sum - sum_squares)
 }
