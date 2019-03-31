9467be0f248b051f730c4d1662f5ba7359468689
Jon Calder <jonmcalder@gmail.com>
2019-03-31 15:56:42 +0200

    Update acronym example.R to pass new tests

--- exercises/acronym/example.R
+++ exercises/acronym/example.R
@@ -1,12 +1,6 @@
 acronym <- function(input) {
-  # split the string by spaces or hyphens
-  split <- strsplit(input, " |-")
-  
-  # get the first letter of each substring
-  first_letters <- substring(split[[1]], 1, 1)
-
-  # join the acronym back together & make uppercase
-  acronym <- paste(toupper(first_letters), collapse = "")
-  
-  return(acronym)
+  words <- strsplit(input, " |-")[[1]]
+  words <- gsub("[[:punct:]]", "", words)
+  chrs <- lapply(words, substring, 1, 1)
+  toupper(paste(chrs, collapse = ""))
 }
