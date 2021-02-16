5f29a49b4b417b6df0fbd709637ad9ae3a5f0720
Jon Calder <jonmcalder@gmail.com>
2021-02-16 19:32:11 +0200

    Update linting and testing 👷‍♂️ (#153)
    * Update linting and testing 👷
    
    * Fix linting in prime-factors 🚨

--- bin/run_tests.R
+++ bin/run_tests.R
@@ -1,40 +1,38 @@
+# Only testing practice exercises for now
 
 test_exercise <- function(exercise) {
-  
   solution_file <- paste0(exercise, ".R")
   test_file <- paste0("test_", exercise, ".R")
-  
+
   file.copy(
-    file.path("..", "exercises", exercise, "example.R"), 
+    file.path("..", "exercises/practice", exercise, ".meta/example.R"),
     solution_file
   )
   file.copy(
-    file.path("..", "exercises", exercise, test_file), 
+    file.path("..", "exercises/practice", exercise, test_file),
     test_file
   )
-  
+
   source(test_file)
   rm(list = ls())
-  
 }
 
 run_tests <- function() {
 
   # create temp directory for testing purposes
-  temp_dir <- "temp" 
+  temp_dir <- "temp"
   dir.create(temp_dir)
   setwd(temp_dir)
-  
+
   on.exit({
     # clean up on exit
     setwd(dir = "../")
     unlink("temp", recursive = TRUE)
   })
-  
+
   # read config and test all exercises
   config <- jsonlite::fromJSON(file.path("..", "config.json"))
-  lapply(config$exercises$slug, test_exercise)
-    
+  lapply(config$exercises$practice$slug, test_exercise)
 }
 
 run_tests()
--- exercises/practice/prime-factors/.meta/example.R
+++ exercises/practice/prime-factors/.meta/example.R
@@ -1,13 +1,13 @@
 prime_factors <- function(number) {
   factors <- c()
-  lowest.factor <- 2
+  lowest_factor <- 2
   while (number > 1) {
-    if (number %% lowest.factor == 0) {
-      number <- number / lowest.factor
-      factors[length(factors) + 1] <- lowest.factor
+    if (number %% lowest_factor == 0) {
+      number <- number / lowest_factor
+      factors[length(factors) + 1] <- lowest_factor
     }
     else {
-      lowest.factor <- lowest.factor + 1
+      lowest_factor <- lowest_factor + 1
     }
   }
   return(factors)
