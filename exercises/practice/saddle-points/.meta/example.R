ced57867509c67e9346ac733044dddcc7ce65eec
Jon Calder <jonmcalder@gmail.com>
2023-03-17 18:21:47 +0200

    :boom: Update saddle-points implementation based on feedback

--- exercises/practice/saddle-points/.meta/example.R
+++ exercises/practice/saddle-points/.meta/example.R
@@ -1,13 +1,11 @@
 saddle_point <- function(input) {
-  points <- data.frame()
-  if (length(input) == 0) {
-    return(points)
-  }
+  points <- data.frame(row = numeric(), col = numeric())
   dims <- dim(input)
-  for (row in 1:dims[1]) {
-    for (col in 1:dims[2]) {
-      if (input[row, col] == min(input[, col]) &&
-        input[row, col] == max(input[row, ])) {
+  for (row in seq_len(dims[1])) {
+    row_max <- max(input[row, ])
+    for (col in seq_len(dims[2])) {
+      if (input[row, col] == row_max &&
+        input[row, col] == min(input[, col])) {
         points <- rbind(points, data.frame(row = row, col = col))
       }
     }
