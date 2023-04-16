df430456ffa0737aa5893cbcb002063515cec3f0
colinleach <colin.leach@comcast.net>
2023-04-16 12:23:30 -0700

    fixed linting issue

--- exercises/practice/armstrong-numbers/.meta/example.R
+++ exercises/practice/armstrong-numbers/.meta/example.R
@@ -1,9 +1,9 @@
 to_digits <- function(n) {
-  chars = n |> as.character() |> strsplit("")
+  chars <- n |> as.character() |> strsplit("")
   as.numeric(chars[[1]])
 }
 
 is_armstrong_number <- function(n) {
-  digits = to_digits(n)
+  digits <- to_digits(n)
   sum(digits ^ length(digits)) == n
 }
