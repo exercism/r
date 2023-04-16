442f41560c94c510dc6dd76247ec02a9475beda8
colinleach <colin.leach@comcast.net>
2023-04-15 17:17:50 -0700

    darts exercise

--- exercises/practice/darts/.meta/example.R
+++ exercises/practice/darts/.meta/example.R
@@ -1,17 +1,4 @@
 score <- function(x, y) {
-  # points <- data.frame(row = numeric(), col = numeric())
-  # dims <- dim(input)
-  # for (row in seq_len(dims[1])) {
-  #   row_max <- max(input[row, ])
-  #   for (col in seq_len(dims[2])) {
-  #     if (input[row, col] == row_max &&
-  #       input[row, col] == min(input[, col])) {
-  #       points <- rbind(points, data.frame(row = row, col = col))
-  #     }
-  #   }
-  # }
-  # points
-
   radius <- sqrt(x^2 + y^2)
   if (radius <= 1) return(10)
   if (radius <= 5) return(5)
