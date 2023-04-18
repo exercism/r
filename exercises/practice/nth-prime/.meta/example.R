7ce5ff3d42adaff1564ab4a7e2844cc48918c0ef
colinleach <colin.leach@comcast.net>
2023-04-17 23:33:48 -0700

    working example code

---
+++ exercises/practice/nth-prime/.meta/example.R
@@ -0,0 +1,35 @@
+# Not the prettiest code, but it passes the tests reasonably quickly
+
+prime <- function(n) {
+  if (n < 1) stop("n must be a positive integer")
+  
+  known <- vector("integer", n) # to store primes as we find them
+  
+  is_prime <- function(candidate) {
+    sqrt_candidate <- sqrt(candidate)
+    for (item in known) {
+      if (item > sqrt_candidate) return(TRUE)
+      if (candidate %% item == 0) return(FALSE)
+      TRUE
+    }
+  }
+  
+  # Early primes are added explicitly:
+  known[1] <- 2
+  known[2] <- 3
+  
+  # Prime candidates are 6i +/- 1 : 5, 7, 11, 13...
+  # Python does this with `itertools.choice()`. Is there an equivalent in R?
+  known_count <- 2
+  number <- 6
+  while (TRUE) {
+    for (candidate in c(number - 1, number + 1)) {
+      if (known_count >= n) return(known[n]) # result!
+      if (is_prime(candidate)) {
+        known_count <- known_count + 1
+        known[known_count] <- candidate
+      }
+    }
+    number <- number + 6
+  }
+}
--- exercises/practice/nth-prime/nth-prime.R
+++ exercises/practice/nth-prime/nth-prime.R
@@ -1,33 +1,3 @@
-
 prime <- function(n) {
-  if (n < 1) stop("n must be a positive integer")
-  
-  known <- vector("integer", n)
-  is_prime <- function(candidate) {
-    sqrt_candidate <- sqrt(candidate)
-    for (item in known) {
-      if (item > sqrt_candidate) return(TRUE)
-      if (candidate %% item == 0) return(FALSE)
-      TRUE
-    }
+
   }
-  
-  known[1] <- 2
-  known[2] <- 3
-  known_count <- 2
-  number <- 6
-  while (TRUE) {
-    if (known_count >= n) return(known[n])
-    if (is_prime(number - 1)) {
-      known_count <- known_count + 1
-      known[known_count] <- number - 1
-    }
-    if (known_count >= n) return(known[n])
-    if (is_prime(number + 1)) {
-      known_count <- known_count + 1
-      known[known_count] <- number + 1
-    }
-    number <- number + 6
-  }
-  
-}
