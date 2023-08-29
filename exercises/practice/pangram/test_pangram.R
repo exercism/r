29c2eb15c571e0541c2c91544b7eab9b146a740c
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2023-08-29 00:31:36 -0700

    Update pangram tests (#289)
    * Updates to pangram tests
    
    * Fix typo

--- exercises/practice/pangram/test_pangram.R
+++ exercises/practice/pangram/test_pangram.R
@@ -3,28 +3,33 @@ library(testthat)
 
 context("pangram")
 
-test_that("sentence empty", {
+test_that("empty sentence", {
     expect_false(is_pangram(""))
 })
 
-test_that("pangram with only lower case", {
+test_that("perfect lower case", {
+    expect_true(is_pangram("abcdefghijklmnopqrstuvwxyz"))
+})
+
+test_that("only lower case", {
     expect_true(is_pangram("the quick brown fox jumps over the lazy dog"))
 })
 
-test_that("missing character 'x'", {
+test_that("missing the letter 'x'", {
     expect_false(
       is_pangram("a quick movement of the enemy will jeopardize five gunboats"))
 })
 
-test_that("another missing character 'x'", {
-    expect_false(is_pangram("the quick brown fish jumps over the lazy dog"))
+test_that("missing the letter 'h'", {
+    expect_false(
+      is_pangram("five boxing wizards jump quickly at it"))
 })
 
-test_that("pangram with underscores", {
+test_that("with underscores", {
     expect_true(is_pangram("the_quick_brown_fox_jumps_over_the_lazy_dog"))
 })
 
-test_that("pangram with numbers", {
+test_that("with numbers", {
     expect_true(is_pangram("the 1 quick brown fox jumps over the 2 lazy dogs"))
 })
 
@@ -32,13 +37,12 @@ test_that("missing letters replaced by numbers", {
     expect_false(is_pangram("7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog"))
 })
 
-test_that("pangram with mixed case and punctuation", {
+test_that("mixed case and punctuation", {
     expect_true(is_pangram("\"Five quacking Zephyrs jolt my wax bed.\""))
 })
 
-test_that("upper and lower case versions of the same character should not be 
-          counted separately", {
-    expect_false(is_pangram("the quick brown fox jumped over the lazy FOX"))
+test_that("a-m and A-M are 26 different characters but not a pangram", {
+    expect_false(is_pangram("abcdefghijklm ABCDEFGHIJKLM"))
 })
 
 message("All tests passed for exercise: pangram")
