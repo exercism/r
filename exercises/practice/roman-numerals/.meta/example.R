47ea35de0acdd1d74939f1186269e63a5628313c
colinleach <colin.leach@comcast.net>
2024-01-21 17:27:23 -0700

    replaced example.R with something simpler

--- exercises/practice/roman-numerals/.meta/example.R
+++ exercises/practice/roman-numerals/.meta/example.R
@@ -1,16 +1,13 @@
-library(tibble)
-
 roman <- function(arabic) {
-  nums  <-   c(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
-  romans  <-  c("M", "CM", "D", "CD", "C", "XC",
-                "L", "XL", "X", "IX", "V", "IV", "I")
-  lookup <- tibble(nums, romans)
-  result <-  c()
-
-  for (i in seq_len(nrow(lookup))) {
-    num <- lookup[i, "nums"][[1]]
+  nums  <- c(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+  romans  <- c("M", "CM", "D", "CD", "C", "XC",
+               "L", "XL", "X", "IX", "V", "IV", "I")
+  result <- c()
+  
+  for (i in 1:length(nums)) {
+    num <- nums[i]
     while (num <= arabic) {
-      result <- append(result, lookup[i, "romans"][[1]])
+      result <- append(result, romans[i])
       arabic <- arabic - num
     }
   }
