9a05c8b4ad70faa9aef83d09809dc78b7e049236
Nerwosolek <24649931+Nerwosolek@users.noreply.github.com>
2024-04-18 23:54:31 +0200

    example.R reformated for linter

--- exercises/practice/phone-number/.meta/example.R
+++ exercises/practice/phone-number/.meta/example.R
@@ -1,7 +1,10 @@
 parse_phone_number <- function(number_string) {
   str <- gsub("\\D", replacement = "", number_string)
-  if (substr(str, 1, 1) == '1') str <- substr(str, 2, nchar(str))
-  if (nchar(str) != 10) return(NULL)
-  if (strtoi(substr(str, 1, 1)) < 2 || strtoi(substr(str, 4, 4)) < 2) return(NULL)
+  if (substr(str, 1, 1) == '1') 
+    str <- substr(str, 2, nchar(str))
+  if (nchar(str) != 10) 
+    return(NULL)
+  if (strtoi(substr(str, 1, 1)) < 2 || strtoi(substr(str, 4, 4)) < 2) 
+    return(NULL)
   str
 }
