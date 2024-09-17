8ad8655b6120d7b7985e7c2ab35ac8b7bd99d255
Erik Schierboom <erik_schierboom@hotmail.com>
2024-09-17 17:08:35 +0200

    Minor fixes

--- exercises/practice/game-of-life/test_game-of-life.R
+++ exercises/practice/game-of-life/test_game-of-life.R
@@ -2,123 +2,109 @@ source("./game-of-life.R")
 library(testthat)
 
 test_that("live cells with zero live neighbors die", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(0, 0, 0,
-            0, 1, 0,
-            0, 0, 0), 3, byrow = TRUE)),
+  expect_equal(
+    tick(
       matrix(
         c(0, 0, 0,
-          0, 0, 0,
-          0, 0, 0), 3, byrow = TRUE)
-    )
+          0, 1, 0,
+          0, 0, 0), 3, byrow = TRUE)),
+    matrix(
+      c(0, 0, 0,
+        0, 0, 0,
+        0, 0, 0), 3, byrow = TRUE)
   )
 })
 
 test_that("live cells with only one live neighbor die", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(0, 0, 0,
-            0, 1, 0,
-            0, 1, 0), 3, byrow = TRUE)),
+  expect_equal(
+    tick(
       matrix(
         c(0, 0, 0,
-          0, 0, 0,
-          0, 0, 0), 3, byrow = TRUE)
-    )
+          0, 1, 0,
+          0, 1, 0), 3, byrow = TRUE)),
+    matrix(
+      c(0, 0, 0,
+        0, 0, 0,
+        0, 0, 0), 3, byrow = TRUE)
   )
 })
 
 test_that("live cells with two live neighbors stay alive", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(1, 0, 1,
-            1, 0, 1,
-            1, 0, 1), 3, byrow = TRUE)),
+  expect_equal(
+    tick(
       matrix(
-        c(0, 0, 0,
+        c(1, 0, 1,
           1, 0, 1,
-          0, 0, 0), 3, byrow = TRUE)
+          1, 0, 1), 3, byrow = TRUE)),
+    matrix(
+      c(0, 0, 0,
+        1, 0, 1,
+        0, 0, 0), 3, byrow = TRUE)
     )
-  )
 })
 
 test_that("live cells with three live neighbors stay alive", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(0, 1, 0, 
-            1, 0, 0, 
-            1, 1, 0), 3, byrow = TRUE)),
+  expect_equal(
+    tick(
       matrix(
-        c(0, 0, 0, 
+        c(0, 1, 0, 
           1, 0, 0, 
-          1, 1, 0), 3, byrow = TRUE)
-    )
+          1, 1, 0), 3, byrow = TRUE)),
+    matrix(
+      c(0, 0, 0, 
+        1, 0, 0, 
+        1, 1, 0), 3, byrow = TRUE)
   )
 })
 
 test_that("dead cells with three live neighbors become alive", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(1, 1, 0, 
-            0, 0, 0, 
-            1, 0, 0), 3, byrow = TRUE)),
+  expect_equal(
+    tick(
       matrix(
-        c(0, 0, 0,
-          1, 1, 0,
-          0, 0, 0), 3, byrow = TRUE)
-    )
+        c(1, 1, 0, 
+          0, 0, 0, 
+          1, 0, 0), 3, byrow = TRUE)),
+    matrix(
+      c(0, 0, 0,
+        1, 1, 0,
+        0, 0, 0), 3, byrow = TRUE)
   )
 })
 
 test_that("live cells with four or more neighbors die", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(1, 1, 1, 
-            1, 1, 1, 
-            1, 1, 1), 3, byrow = TRUE)),
+  expect_equal(
+    tick(
       matrix(
-        c(1, 0, 1, 
-          0, 0, 0, 
-          1, 0, 1), 3, byrow = TRUE)
-    )
+        c(1, 1, 1, 
+          1, 1, 1, 
+          1, 1, 1), 3, byrow = TRUE)),
+    matrix(
+      c(1, 0, 1, 
+        0, 0, 0, 
+        1, 0, 1), 3, byrow = TRUE)
   )
 })
 
 test_that("bigger matrix", {
-  expect_true(
-    all.equal(
-      tick(
-        matrix(
-          c(1, 1, 0, 1, 1, 0, 0, 0, 
-            1, 0, 1, 1, 0, 0, 0, 0, 
-            1, 1, 1, 0, 0, 1, 1, 1, 
-            0, 0, 0, 0, 0, 1, 1, 0, 
-            1, 0, 0, 0, 1, 1, 0, 0, 
-            1, 1, 0, 0, 0, 1, 1, 1, 
-            0, 0, 1, 0, 1, 0, 0, 1, 
-            1, 0, 0, 0, 0, 0, 1, 1), 8)),
+  expect_equal(
+    tick(
       matrix(
         c(1, 1, 0, 1, 1, 0, 0, 0, 
+          1, 0, 1, 1, 0, 0, 0, 0, 
+          1, 1, 1, 0, 0, 1, 1, 1, 
           0, 0, 0, 0, 0, 1, 1, 0, 
-          1, 0, 1, 1, 1, 1, 0, 1,
-          1, 0, 0, 0, 0, 0, 0, 1, 
-          1, 1, 0, 0, 1, 0, 0, 1, 
-          1, 1, 0, 1, 0, 0, 0, 1,
-          1, 0, 0, 0, 0, 0, 0, 0, 
-          0, 0, 0, 0, 0, 0, 1, 1), 8)
-    )
+          1, 0, 0, 0, 1, 1, 0, 0, 
+          1, 1, 0, 0, 0, 1, 1, 1, 
+          0, 0, 1, 0, 1, 0, 0, 1, 
+          1, 0, 0, 0, 0, 0, 1, 1), 8)),
+    matrix(
+      c(1, 1, 0, 1, 1, 0, 0, 0, 
+        0, 0, 0, 0, 0, 1, 1, 0, 
+        1, 0, 1, 1, 1, 1, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 1, 
+        1, 1, 0, 0, 1, 0, 0, 1, 
+        1, 1, 0, 1, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 
+        0, 0, 0, 0, 0, 0, 1, 1), 8)
   )
 })
