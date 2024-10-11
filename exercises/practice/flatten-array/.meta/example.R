3bc626ab0e5164783f9c03b433ff941758c40257
Erik Schierboom <erik_schierboom@hotmail.com>
2024-10-11 19:59:15 +0200

    flatten-array: fix nesting (#358)
    * flatten-array: fix nesting
    
    * Update example.R
    
    ---------
    
    Co-authored-by: colinleach <colin.leach@comcast.net>

--- exercises/practice/flatten-array/.meta/example.R
+++ exercises/practice/flatten-array/.meta/example.R
@@ -1,3 +1,14 @@
 flatten <- function(input) {
-  input[!is.null(input)]
+  flattened <- c()
+  
+  for (element in input) {
+    if (is.null(element)) next
+    
+    if (is.list(element))
+      flattened <- c(flattened, flatten(element))
+    else
+      flattened <- c(flattened, element)
+  }
+  
+  flattened
 }
