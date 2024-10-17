2732ccef4d1f5b9669c18abf0d0e0251e1cf27c6
Erik Schierboom <erik_schierboom@hotmail.com>
2024-10-17 18:51:15 +0200

    Add `list-ops` exercise

---
+++ exercises/practice/list-ops/list-ops.R
@@ -0,0 +1,26 @@
+source("./list-ops.R")
+library(testthat)
+
+my_append <- function(x, y) {
+}
+
+my_concat <- function(x) {
+}
+
+my_filter <- function(x, f) {
+}
+
+my_length <- function(x) {
+}
+
+my_map <- function(x, f) {
+}
+
+my_foldl <- function(x, f, init) {
+}
+
+my_foldr <- function(x, f, init) {
+}
+
+my_reverse <- function(x) {
+}
