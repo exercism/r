8dd27888424fe9f4c906d9786ad3b56d4f47ade4
Colin Leach <colin.leach@comcast.net>
2026-02-18 17:10:49 -0700

    Bird Watcher concept exercise

---
+++ exercises/concept/bird-watcher/bird-watcher.R
@@ -0,0 +1,20 @@
+today <- function(birds_per_day) {
+}
+
+increment_todays_count <- function(birds_per_day) {
+}
+
+has_day_without_birds <- function(birds_per_day) {
+}
+
+count_for_first_days <- function(birds_per_day, num_days) {
+}
+
+busy_days <- function(birds_per_day) {
+}
+
+running_total <- function(birds_per_day) {
+}
+
+busy_days_of_week <- function(birds_per_day, day_names) {
+}
