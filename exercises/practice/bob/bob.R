0c8237f51e4f70ee818c5c256cc65f34f604d1c3
Colin Leach <colin.leach@comcast.net>
2026-03-11 13:31:54 -0700

    Revert "Collatz Conjecture - broken"
    This reverts commit 028db1db5d1df7f933a891b12eda4865c051bc1a.

--- exercises/practice/bob/bob.R
+++ exercises/practice/bob/bob.R
@@ -1,27 +1,3 @@
-# bob <- function(input) {
-# 
-# }
-
 bob <- function(input) {
-  # function bob takes a character input and responds
-  # with a couple different sentences
-  
-  # strip white space
-  input <- gsub("[[:space:]]", "", input)
-  
-  if (input == "") {
-    return("Fine. Be that way!")
-  }
-  if (toupper(input) == input && 
-      tolower(input) != input && 
-      endsWith(input, "?")) {
-    return("Calm down, I know what I'm doing!")
-  }
-  if (toupper(input) == input && tolower(input) != input) {
-    return("Whoa, chill out!")
-  }
-  if (endsWith(input, "?")) {
-    return("Sure.")
-  }
-  return("Whatever.")
+
 }
--- exercises/practice/collatz-conjecture/test_collatz-conjecture.R
+++ exercises/practice/collatz-conjecture/test_collatz-conjecture.R
@@ -1,30 +1,32 @@
-# These tests are auto-generated with test data from:
-# https://github.com/exercism/problem-specifications/tree/main/exercises/collatz-conjecture/canonical-data.json
-# File last updated on 2026-03-11
-
 source("./collatz-conjecture.R")
 library(testthat)
 
-test_that("zero steps for one", {
-  expect_equal(steps(1), 0)
+test_that("Input of 1 results in 0 steps", {
+  expect_equal(collatz_step_counter(1), 0)
 })
 
-test_that("divide if even", {
-  expect_equal(steps(16), 4)
+test_that("Even input with repeated even steps", {
+  expect_equal(collatz_step_counter(16), 4)
 })
 
-test_that("even and odd steps", {
-  expect_equal(steps(12), 9)
+test_that("Input which results in small number of even and odd steps", {
+  expect_equal(collatz_step_counter(12), 9)
 })
 
-test_that("large number of even and odd steps", {
-  expect_equal(steps(1000000), 152)
+test_that("Input which results in large number of even and odd steps", {
+  expect_equal(collatz_step_counter(1000000), 152)
 })
 
-test_that("zero is an error", {
-  expect_error(steps(0))
+test_that("Input of 0 results in an error", {
+  expect_error(collatz_step_counter(0))
 })
 
-test_that("negative value is an error", {
-  expect_error(steps(-15))
+test_that("Negative input results in an error", {
+  expect_error(collatz_step_counter(-15))
+})
+
+test_that("Answer can accept vector parameter", {
+  expect_equal(collatz_step_counter(
+    c(1, 16, 12, 1000000)
+  ), (c(0, 4, 9, 152)))
 })
