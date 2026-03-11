373dd083dada96572a000485ce363c240c03f14d
Colin Leach <colinleach@pm.me>
2026-03-11 16:55:06 -0700

    [Add template] Collatz Conjecture draft (#472)
    * Collatz Conjecture - broken
    
    * Revert "Collatz Conjecture - broken"
    
    This reverts commit 028db1db5d1df7f933a891b12eda4865c051bc1a.
    
    * Failing collatz-conjecture template
    
    * template fix
    
    * use the correct function name
    
    * revised template

--- exercises/practice/collatz-conjecture/test_collatz-conjecture.R
+++ exercises/practice/collatz-conjecture/test_collatz-conjecture.R
@@ -1,32 +1,32 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/collatz-conjecture/canonical-data.json
+# File last updated on 2026-03-11
+
 source("./collatz-conjecture.R")
 library(testthat)
 
-test_that("Input of 1 results in 0 steps", {
+test_that("zero steps for one", {
   expect_equal(collatz_step_counter(1), 0)
 })
-
-test_that("Even input with repeated even steps", {
+test_that("divide if even", {
   expect_equal(collatz_step_counter(16), 4)
 })
-
-test_that("Input which results in small number of even and odd steps", {
+test_that("even and odd steps", {
   expect_equal(collatz_step_counter(12), 9)
 })
-
-test_that("Input which results in large number of even and odd steps", {
+test_that("large number of even and odd steps", {
   expect_equal(collatz_step_counter(1000000), 152)
 })
-
-test_that("Input of 0 results in an error", {
+test_that("zero is an error", {
   expect_error(collatz_step_counter(0))
 })
-
-test_that("Negative input results in an error", {
+test_that("negative value is an error", {
   expect_error(collatz_step_counter(-15))
 })
 
-test_that("Answer can accept vector parameter", {
-  expect_equal(collatz_step_counter(
-    c(1, 16, 12, 1000000)
-  ), (c(0, 4, 9, 152)))
+
+# Additional tests for this track
+
+test_that("answer can accept vector parameter", {
+  expect_equal(collatz_step_counter(c(1, 16, 12, 1000000)), c(0, 4, 9, 152))
 })
