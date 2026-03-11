a87f8ce23a55a82f7043d217c5151c30a6b74f75
Colin Leach <colin.leach@comcast.net>
2026-03-11 15:47:15 -0700

    revised template

--- exercises/practice/collatz-conjecture/test_collatz-conjecture.R
+++ exercises/practice/collatz-conjecture/test_collatz-conjecture.R
@@ -6,25 +6,27 @@ source("./collatz-conjecture.R")
 library(testthat)
 
 test_that("zero steps for one", {
-  expect_equal(steps(1), 0)
+  expect_equal(collatz_step_counter(1), 0)
 })
-
 test_that("divide if even", {
-  expect_equal(steps(16), 4)
+  expect_equal(collatz_step_counter(16), 4)
 })
-
 test_that("even and odd steps", {
-  expect_equal(steps(12), 9)
+  expect_equal(collatz_step_counter(12), 9)
 })
-
 test_that("large number of even and odd steps", {
-  expect_equal(steps(1000000), 152)
+  expect_equal(collatz_step_counter(1000000), 152)
 })
-
 test_that("zero is an error", {
-  expect_error(steps(0))
+  expect_error(collatz_step_counter(0))
+})
+test_that("negative value is an error", {
+  expect_error(collatz_step_counter(-15))
 })
 
-test_that("negative value is an error", {
-  expect_error(steps(-15))
+
+# Additional tests for this track
+
+test_that("answer can accept vector parameter", {
+  expect_equal(collatz_step_counter(c(1, 16, 12, 1000000)), c(0, 4, 9, 152))
 })
