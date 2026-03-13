4afae64b92436d701f974c5f235a73315e5fc8e0
Colin Leach <colinleach@pm.me>
2026-03-13 11:00:55 -0700

    [Add template] Flatten Array (#479)
    * partial update
    
    * [Add template] flatten-array
    
    * array -> list in descriptions

--- exercises/practice/flatten-array/OLD_test_flatten-array.R
+++ exercises/practice/flatten-array/OLD_test_flatten-array.R
@@ -1,7 +1,3 @@
-# These tests are auto-generated with test data from:
-# https://github.com/exercism/problem-specifications/tree/main/exercises/flatten-array/canonical-data.json
-# File last updated on 2026-03-13
-
 source("./flatten-array.R")
 library(testthat)
 
@@ -17,13 +13,13 @@ test_that("no nesting", {
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("flattens a nested list", {
+test_that("flattens a nested array", {
   inputs <- list(list(list()))
   expected <- c()
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("flattens list with just integers present", {
+test_that("flattens array with just integers present", {
   inputs <- list(1, list(2, 3, 4, 5, 6, 7), 8)
   expected <- c(1, 2, 3, 4, 5, 6, 7, 8)
   expect_equal(flatten(inputs), expected)
@@ -47,38 +43,26 @@ test_that("null values are omitted from the final result", {
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("consecutive null values at the front of the list are omitted from the final result", {
+test_that("consecutive null values at the front of the list are omitted from the final result", { # nolint
   inputs <- list(NULL, NULL, 3)
   expected <- c(3)
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("consecutive null values in the middle of the list are omitted from the final result", {
+test_that("consecutive null values in the middle of the list are omitted from the final result", { # nolint
   inputs <- list(1, NULL, NULL, 4)
   expected <- c(1, 4)
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("6 level nested list with null values", {
-  inputs <- list(
-    0,
-    2,
-    list(list(2, 3), 8, list(list(100)), NULL, list(list(NULL))),
-    -2
-  )
+test_that("6 level nest list with null values", {
+  inputs <- list(0, 2, list(list(2, 3), 8, list(list(100)), NULL, list(list(NULL))), -2) # nolint
   expected <- c(0, 2, 2, 3, 8, 100, -2)
   expect_equal(flatten(inputs), expected)
 })
 
 test_that("all values in nested list are null", {
-  inputs <- list(
-    NULL,
-    list(list(list(NULL))),
-    NULL,
-    NULL,
-    list(list(NULL, NULL), NULL),
-    NULL
-  )
+  inputs <- list(NULL, list(list(list(NULL))), NULL, NULL, list(list(NULL, NULL), NULL), NULL) # nolint
   expected <- c()
   expect_equal(flatten(inputs), expected)
 })
