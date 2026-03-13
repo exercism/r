daea6d72cdaac99ec441ca49ec47a0b15953ba16
Colin Leach <colinleach@pm.me>
2026-03-13 08:32:27 -0700

    Fix template updates (#478)  [no important files changed]
    * Collatz Conjecture - broken
    
    * Revert "Collatz Conjecture - broken"
    
    This reverts commit 028db1db5d1df7f933a891b12eda4865c051bc1a.
    
    * fix template updates

--- exercises/practice/collatz-conjecture/test_collatz-conjecture.R
+++ exercises/practice/collatz-conjecture/test_collatz-conjecture.R
@@ -1,25 +1,31 @@
 # These tests are auto-generated with test data from:
 # https://github.com/exercism/problem-specifications/tree/main/exercises/collatz-conjecture/canonical-data.json
-# File last updated on 2026-03-11
+# File last updated on 2026-03-13
 
 source("./collatz-conjecture.R")
 library(testthat)
 
+
 test_that("zero steps for one", {
   expect_equal(collatz_step_counter(1), 0)
 })
+
 test_that("divide if even", {
   expect_equal(collatz_step_counter(16), 4)
 })
+
 test_that("even and odd steps", {
   expect_equal(collatz_step_counter(12), 9)
 })
+
 test_that("large number of even and odd steps", {
   expect_equal(collatz_step_counter(1000000), 152)
 })
+
 test_that("zero is an error", {
   expect_error(collatz_step_counter(0))
 })
+
 test_that("negative value is an error", {
   expect_error(collatz_step_counter(-15))
 })
