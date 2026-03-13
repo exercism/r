50f64d2e1047e603127fb9d7b1c6e87a9ea158d7
Colin Leach <colin.leach@comcast.net>
2026-03-13 10:39:38 -0700

    add template

--- exercises/practice/isbn-verifier/test_isbn-verifier.R
+++ exercises/practice/isbn-verifier/test_isbn-verifier.R
@@ -1,27 +1,31 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/isbn-verifier/canonical-data.json
+# File last updated on 2026-03-13
+
 source("./isbn-verifier.R")
 library(testthat)
 
-test_that("Valid isbn", {
+test_that("valid isbn", {
   expect_true(is_valid("3-598-21508-8"))
 })
 
-test_that("Invalid isbn check digit", {
+test_that("invalid isbn check digit", {
   expect_false(is_valid("3-598-21508-9"))
 })
 
-test_that("Valid isbn with a check digit of 10", {
+test_that("valid isbn with a check digit of 10", {
   expect_true(is_valid("3-598-21507-X"))
 })
 
-test_that("Check digit is a character other than X", {
+test_that("check digit is a character other than X", {
   expect_false(is_valid("3-598-21507-A"))
 })
 
-test_that("Invalid check digit in isbn is not treated as zero", {
+test_that("invalid check digit in isbn is not treated as zero", {
   expect_false(is_valid("4-598-21507-B"))
 })
 
-test_that("Invalid character in isbn is not treated as zero", {
+test_that("invalid character in isbn is not treated as zero", {
   expect_false(is_valid("3-598-P1581-X"))
 })
 
@@ -29,50 +33,58 @@ test_that("X is only valid as a check digit", {
   expect_false(is_valid("3-598-2X507-9"))
 })
 
-test_that("Valid isbn without separating dashes", {
+test_that("only one check digit is allowed", {
+  expect_false(is_valid("3-598-21508-96"))
+})
+
+test_that("X is not substituted by the value 10", {
+  expect_false(is_valid("3-598-2X507-5"))
+})
+
+test_that("valid isbn without separating dashes", {
   expect_true(is_valid("3598215088"))
 })
 
-test_that("Isbn without separating dashes and X as check digit", {
+test_that("isbn without separating dashes and X as check digit", {
   expect_true(is_valid("359821507X"))
 })
 
-test_that("Isbn without check digit and dashes", {
+test_that("isbn without check digit and dashes", {
   expect_false(is_valid("359821507"))
 })
 
-test_that("Too long isbn and no dashes", {
+test_that("too long isbn and no dashes", {
   expect_false(is_valid("3598215078X"))
 })
 
-test_that("Too short isbn", {
+test_that("too short isbn", {
   expect_false(is_valid("00"))
 })
 
-test_that("Isbn without check digit", {
+test_that("isbn without check digit", {
   expect_false(is_valid("3-598-21507"))
 })
 
-test_that("Check digit of X should not be used for 0", {
+test_that("check digit of X should not be used for 0", {
   expect_false(is_valid("3-598-21515-X"))
 })
 
-test_that("Empty isbn", {
+test_that("empty isbn", {
   expect_false(is_valid(""))
 })
 
-test_that("Input is 9 characters", {
+test_that("input is 9 characters", {
   expect_false(is_valid("134456729"))
 })
 
-test_that("Invalid characters are not ignored after checking length", {
+test_that("invalid characters are not ignored after checking length", {
   expect_false(is_valid("3132P34035"))
 })
 
-test_that("Invalid characters are not ignored before checking length", {
+test_that("invalid characters are not ignored before checking length", {
   expect_false(is_valid("3598P215088"))
 })
 
-test_that("Input is too long but contains a valid isbn", {
+test_that("input is too long but contains a valid isbn", {
   expect_false(is_valid("98245726788"))
 })
