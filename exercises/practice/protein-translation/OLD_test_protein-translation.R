49b9984bfd5e6be500a7551029a33cedb456c993
depial <91621102+depial@users.noreply.github.com>
2026-03-13 19:57:45 -0400

    Add protein-translation template and update tests.toml (#483)
    [no important files changed]

--- exercises/practice/protein-translation/OLD_test_protein-translation.R
+++ exercises/practice/protein-translation/OLD_test_protein-translation.R
@@ -1,68 +1,64 @@
-# These tests are auto-generated with test data from:
-# https://github.com/exercism/problem-specifications/tree/main/exercises/protein-translation/canonical-data.json
-# File last updated on 2026-03-13
-
 source("./protein-translation.R")
 library(testthat)
 
 test_that("Empty RNA sequence results in no proteins", {
-  expect_equal(translate(""), c())
+  expect_null(translate(""))
 })
 
 test_that("Methionine RNA sequence", {
-  expect_equal(translate("AUG"), c('Methionine'))
+  expect_equal(translate("AUG"), "Methionine")
 })
 
 test_that("Phenylalanine RNA sequence 1", {
-  expect_equal(translate("UUU"), c('Phenylalanine'))
+  expect_equal(translate("UUU"), "Phenylalanine")
 })
 
 test_that("Phenylalanine RNA sequence 2", {
-  expect_equal(translate("UUC"), c('Phenylalanine'))
+  expect_equal(translate("UUC"), "Phenylalanine")
 })
 
 test_that("Leucine RNA sequence 1", {
-  expect_equal(translate("UUA"), c('Leucine'))
+  expect_equal(translate("UUA"), "Leucine")
 })
 
 test_that("Leucine RNA sequence 2", {
-  expect_equal(translate("UUG"), c('Leucine'))
+  expect_equal(translate("UUG"), "Leucine")
 })
 
 test_that("Serine RNA sequence 1", {
-  expect_equal(translate("UCU"), c('Serine'))
+  expect_equal(translate("UCU"), "Serine")
 })
 
 test_that("Serine RNA sequence 2", {
-  expect_equal(translate("UCC"), c('Serine'))
+  expect_equal(translate("UCC"), "Serine")
 })
 
 test_that("Serine RNA sequence 3", {
-  expect_equal(translate("UCA"), c('Serine'))
+  expect_equal(translate("UCA"), "Serine")
 })
 
 test_that("Serine RNA sequence 4", {
-  expect_equal(translate("UCG"), c('Serine'))
+  expect_equal(translate("UCG"), "Serine")
 })
 
 test_that("Tyrosine RNA sequence 1", {
-  expect_equal(translate("UAU"), c('Tyrosine'))
+  expect_equal(translate("UAU"), "Tyrosine")
 })
 
 test_that("Tyrosine RNA sequence 2", {
-  expect_equal(translate("UAC"), c('Tyrosine'))
+  expect_equal(translate("UAC"), "Tyrosine")
 })
 
 test_that("Cysteine RNA sequence 1", {
-  expect_equal(translate("UGU"), c('Cysteine'))
+  expect_equal(translate("UGU"), "Cysteine")
 })
 
 test_that("Cysteine RNA sequence 2", {
-  expect_equal(translate("UGC"), c('Cysteine'))
+  expect_equal(translate("UGC"), "Cysteine")
 })
 
 test_that("Tryptophan RNA sequence", {
-  expect_equal(translate("UGG"), c('Tryptophan'))
+  expect_equal(translate("UGG"), "Tryptophan")
 })
 
 test_that("STOP codon RNA sequence 1", {
@@ -78,17 +74,17 @@ test_that("STOP codon RNA sequence 3", {
 })
 
 test_that("Sequence of two protein codons translates into proteins", {
-  expect_equal(translate("UUUUUU"), c('Phenylalanine', 'Phenylalanine'))
+  expect_equal(translate("UUUUUU"), c("Phenylalanine", "Phenylalanine"))
 })
 
 test_that("Sequence of two different protein codons translates into proteins", {
-  expect_equal(translate("UUAUUG"), c('Leucine', 'Leucine'))
+  expect_equal(translate("UUAUUG"), c("Leucine", "Leucine"))
 })
 
 test_that("Translate RNA strand into correct protein list", {
   expect_equal(
     translate("AUGUUUUGG"),
-    c('Methionine', 'Phenylalanine', 'Tryptophan')
+    c("Methionine", "Phenylalanine", "Tryptophan")
   )
 })
 
@@ -97,26 +93,22 @@ test_that("Translation stops if STOP codon at beginning of sequence", {
 })
 
 test_that("Translation stops if STOP codon at end of two-codon sequence", {
-  expect_equal(translate("UGGUAG"), c('Tryptophan'))
+  expect_equal(translate("UGGUAG"), c("Tryptophan"))
 })
 
 test_that("Translation stops if STOP codon at end of three-codon sequence", {
-  expect_equal(translate("AUGUUUUAA"), c('Methionine', 'Phenylalanine'))
+  expect_equal(translate("AUGUUUUAA"), c("Methionine", "Phenylalanine"))
 })
 
 test_that("Translation stops if STOP codon in middle of three-codon sequence", {
-  expect_equal(translate("UGGUAGUGG"), c('Tryptophan'))
+  expect_equal(translate("UGGUAGUGG"), c("Tryptophan"))
 })
 
 test_that("Translation stops if STOP codon in middle of six-codon sequence", {
   expect_equal(
     translate("UGGUGUUAUUAAUGGUUU"),
-    c('Tryptophan', 'Cysteine', 'Tyrosine')
+    c("Tryptophan", "Cysteine", "Tyrosine")
   )
-})
-
-test_that("Sequence of two non-STOP codons does not translate to a STOP codon", {
-  expect_equal(translate("AUGAUG"), c('Methionine', 'Methionine'))
 })
 
 test_that("Non-existing codon can't translate", {
@@ -132,5 +124,5 @@ test_that("Incomplete RNA sequence can't translate", {
 })
 
 test_that("Incomplete RNA sequence can translate if valid until a STOP codon", {
-  expect_equal(translate("UUCUUCUAAUGGU"), c('Phenylalanine', 'Phenylalanine'))
+  expect_equal(translate("UUCUUCUAAUGGU"), c("Phenylalanine", "Phenylalanine"))
 })
