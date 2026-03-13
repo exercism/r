f83cf87ec023b7ac0163052611f2689adb100833
Colin Leach <colin.leach@comcast.net>
2026-03-13 10:18:19 -0700

    array -> list in descriptions

--- exercises/practice/flatten-array/test_flatten-array.R
+++ exercises/practice/flatten-array/test_flatten-array.R
@@ -17,13 +17,13 @@ test_that("no nesting", {
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("flattens a nested array", {
+test_that("flattens a nested list", {
   inputs <- list(list(list()))
   expected <- c()
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("flattens array with just integers present", {
+test_that("flattens list with just integers present", {
   inputs <- list(1, list(2, 3, 4, 5, 6, 7), 8)
   expected <- c(1, 2, 3, 4, 5, 6, 7, 8)
   expect_equal(flatten(inputs), expected)
@@ -47,19 +47,19 @@ test_that("null values are omitted from the final result", {
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("consecutive null values at the front of the array are omitted from the final result", {
+test_that("consecutive null values at the front of the list are omitted from the final result", {
   inputs <- list(NULL, NULL, 3)
   expected <- c(3)
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("consecutive null values in the middle of the array are omitted from the final result", {
+test_that("consecutive null values in the middle of the list are omitted from the final result", {
   inputs <- list(1, NULL, NULL, 4)
   expected <- c(1, 4)
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("6 level nested array with null values", {
+test_that("6 level nested list with null values", {
   inputs <- list(
     0,
     2,
@@ -70,7 +70,7 @@ test_that("6 level nested array with null values", {
   expect_equal(flatten(inputs), expected)
 })
 
-test_that("all values in nested array are null", {
+test_that("all values in nested list are null", {
   inputs <- list(
     NULL,
     list(list(list(NULL))),
