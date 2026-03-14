7dfa824b11e05f4d9fc3ed7be19bd793a656312e
depial <91621102+depial@users.noreply.github.com>
2026-03-14 12:36:59 -0400

    add templates for allergies and perfect-numbers

--- exercises/practice/perfect-numbers/OLD_test_perfect-numbers.R
+++ exercises/practice/perfect-numbers/OLD_test_perfect-numbers.R
@@ -1,94 +1,73 @@
-# These tests are auto-generated with test data from:
-# https://github.com/exercism/problem-specifications/tree/main/exercises/perfect-numbers/canonical-data.json
-# File last updated on 2026-03-14
-
 source("./perfect-numbers.R")
 library(testthat)
 
-
-# Perfect numbers
 test_that("Smallest perfect number is classified correctly", {
   n <- 6
   expect_equal(number_type(n), "perfect")
 })
 
-# Perfect numbers
 test_that("Medium perfect number is classified correctly", {
   n <- 28
   expect_equal(number_type(n), "perfect")
 })
 
-# Perfect numbers
 test_that("Large perfect number is classified correctly", {
   n <- 33550336
   expect_equal(number_type(n), "perfect")
 })
 
 
-# Abundant numbers
+
 test_that("Smallest abundant number is classified correctly", {
   n <- 12
   expect_equal(number_type(n), "abundant")
 })
 
-# Abundant numbers
 test_that("Medium abundant number is classified correctly", {
   n <- 30
   expect_equal(number_type(n), "abundant")
 })
 
-# Abundant numbers
 test_that("Large abundant number is classified correctly", {
   n <- 33550335
   expect_equal(number_type(n), "abundant")
 })
 
-# Abundant numbers
-test_that("Perfect square abundant number is classified correctly", {
-  n <- 196
-  expect_equal(number_type(n), "abundant")
-})
 
 
-# Deficient numbers
 test_that("Smallest prime deficient number is classified correctly", {
   n <- 2
   expect_equal(number_type(n), "deficient")
 })
 
-# Deficient numbers
 test_that("Smallest non-prime deficient number is classified correctly", {
   n <- 4
   expect_equal(number_type(n), "deficient")
 })
 
-# Deficient numbers
 test_that("Medium deficient number is classified correctly", {
   n <- 32
   expect_equal(number_type(n), "deficient")
 })
 
-# Deficient numbers
 test_that("Large deficient number is classified correctly", {
   n <- 33550337
   expect_equal(number_type(n), "deficient")
 })
 
-# Deficient numbers
 test_that("Edge case (no factors other than itself) is classified correctly", {
   n <- 1
   expect_equal(number_type(n), "deficient")
 })
 
 
-# Invalid inputs
-test_that("Zero is rejected (as it is not a positive integer)", {
+
+test_that("Zero is rejected (not a natural number)", {
   n <- 0
   expect_error(number_type(n))
 })
 
-# Invalid inputs
-test_that("Negative integer is rejected (as it is not a positive integer)", {
+test_that("Negative integer is rejected (not a natural number)", {
   n <- -1
   expect_error(number_type(n))
 })
