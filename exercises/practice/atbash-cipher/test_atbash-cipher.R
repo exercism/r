4ab141913a741e422f1bd441530fff1f71b87719
depial <91621102+depial@users.noreply.github.com>
2026-03-16 12:09:04 -0400

    Templates for: all-your-base, atbash-cipher and binary-search (#491)
    * add templates for all-your-base, atbash-cipher, binary-search
    
    * correct variable name in atbash-cipher
    
    * format all-your-base config.json
    
    [no important files changed]

--- exercises/practice/atbash-cipher/test_atbash-cipher.R
+++ exercises/practice/atbash-cipher/test_atbash-cipher.R
@@ -1,85 +1,95 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/atbash-cipher/canonical-data.json
+# File last updated on 2026-03-16
+
 source("./atbash-cipher.R")
 library(testthat)
 
-test_that("Encode yes", {
+
+# encode
+
+test_that("encode yes", {
   plaintext <- "yes"
   expected <- "bvh"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode no", {
+test_that("encode no", {
   plaintext <- "no"
   expected <- "ml"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode OMG", {
+test_that("encode OMG", {
   plaintext <- "OMG"
   expected <- "lnt"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode spaces", {
+test_that("encode spaces", {
   plaintext <- "O M G"
   expected <- "lnt"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode mindblowingly", {
+test_that("encode mindblowingly", {
   plaintext <- "mindblowingly"
   expected <- "nrmwy oldrm tob"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode numbers", {
+test_that("encode numbers", {
   plaintext <- "Testing,1 2 3, testing."
   expected <- "gvhgr mt123 gvhgr mt"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode deep thought", {
+test_that("encode deep thought", {
   plaintext <- "Truth is fiction."
   expected <- "gifgs rhurx grlm"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Encode all the letters", {
+test_that("encode all the letters", {
   plaintext <- "The quick brown fox jumps over the lazy dog."
   expected <- "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
   expect_equal(encode(plaintext), expected)
 })
 
-test_that("Decode exercism", {
+
+# decode
+
+test_that("decode exercism", {
   ciphertext <- "vcvix rhn"
   expected <- "exercism"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode a sentence", {
+test_that("decode a sentence", {
   ciphertext <- "zmlyh gzxov rhlug vmzhg vkkrm thglm v"
   expected <- "anobstacleisoftenasteppingstone"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode numbers", {
+test_that("decode numbers", {
   ciphertext <- "gvhgr mt123 gvhgr mt"
   expected <- "testing123testing"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode all the letters", {
+test_that("decode all the letters", {
   ciphertext <- "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
   expected <- "thequickbrownfoxjumpsoverthelazydog"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode with too many spaces", {
+test_that("decode with too many spaces", {
   ciphertext <- "vc vix    r hn"
   expected <- "exercism"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode with no spaces", {
+test_that("decode with no spaces", {
   ciphertext <- "zmlyhgzxovrhlugvmzhgvkkrmthglmv"
   expected <- "anobstacleisoftenasteppingstone"
   expect_equal(decode(ciphertext), expected)
--- exercises/practice/binary-search/test_binary-search.R
+++ exercises/practice/binary-search/test_binary-search.R
@@ -1,68 +1,72 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/binary-search/canonical-data.json
+# File last updated on 2026-03-16
+
 source("./binary-search.R")
 library(testthat)
 
-test_that("Finds a needle in an haystack with one element", {
+
+test_that("finds a value in an array with one element", {
   haystack <- c(6)
   needle <- 6
   expect_equal(find(haystack, needle), 1)
 })
 
-test_that("Finds a needle in the middle of an haystack", {
+test_that("finds a value in the middle of an array", {
   haystack <- c(1, 3, 4, 6, 8, 9, 11)
   needle <- 6
   expect_equal(find(haystack, needle), 4)
 })
 
-test_that("Finds a needle at the beginning of an haystack", {
+test_that("finds a value at the beginning of an array", {
   haystack <- c(1, 3, 4, 6, 8, 9, 11)
   needle <- 1
   expect_equal(find(haystack, needle), 1)
 })
 
-test_that("Finds a needle at the end of an haystack", {
+test_that("finds a value at the end of an array", {
   haystack <- c(1, 3, 4, 6, 8, 9, 11)
   needle <- 11
   expect_equal(find(haystack, needle), 7)
 })
 
-test_that("Finds a needle in an haystack of odd length", {
+test_that("finds a value in an array of odd length", {
   haystack <- c(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 634)
   needle <- 144
   expect_equal(find(haystack, needle), 10)
 })
 
-test_that("Finds a needle in an haystack of even length", {
+test_that("finds a value in an array of even length", {
   haystack <- c(1, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377)
   needle <- 21
   expect_equal(find(haystack, needle), 6)
 })
 
-test_that("Identifies that a needle is not included in the haystack", {
+test_that("identifies that a value is not included in the array", {
   haystack <- c(1, 3, 4, 6, 8, 9, 11)
   needle <- 7
   expect_equal(find(haystack, needle), -1)
 })
 
-test_that("A needle smaller than the haystack's smallest needle is not found", {
+test_that("a value smaller than the array's smallest value is not found", {
   haystack <- c(1, 3, 4, 6, 8, 9, 11)
   needle <- 0
   expect_equal(find(haystack, needle), -1)
 })
 
-test_that("A needle larger than the haystack's largest needle is not found", {
+test_that("a value larger than the array's largest value is not found", {
   haystack <- c(1, 3, 4, 6, 8, 9, 11)
   needle <- 13
   expect_equal(find(haystack, needle), -1)
 })
 
-test_that("Nothing is found in an empty haystack", {
+test_that("nothing is found in an empty array", {
   haystack <- c()
   needle <- 1
-  expected <- -1
   expect_equal(find(haystack, needle), -1)
 })
 
-test_that("Nothing is found when the left and right bounds cross", {
+test_that("nothing is found when the left and right bounds cross", {
   haystack <- c(1, 2)
   needle <- 0
   expect_equal(find(haystack, needle), -1)
