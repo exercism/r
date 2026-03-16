0c1c93735449b2c680a725e07444bfa12c5102ef
depial <91621102+depial@users.noreply.github.com>
2026-03-16 11:18:47 -0400

    add templates for all-your-base, atbash-cipher, binary-search

--- exercises/practice/atbash-cipher/test_atbash-cipher.R
+++ exercises/practice/atbash-cipher/test_atbash-cipher.R
@@ -1,85 +1,95 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/atbash-cipher/canonical-data.json
+# File last updated on 2026-03-16
+
 source("./atbash-cipher.R")
 library(testthat)
 
-test_that("Encode yes", {
-  plaintext <- "yes"
+
+# encode
+
+test_that("encode yes", {
+  ciphertext <- "yes"
   expected <- "bvh"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode no", {
-  plaintext <- "no"
+test_that("encode no", {
+  ciphertext <- "no"
   expected <- "ml"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode OMG", {
-  plaintext <- "OMG"
+test_that("encode OMG", {
+  ciphertext <- "OMG"
   expected <- "lnt"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode spaces", {
-  plaintext <- "O M G"
+test_that("encode spaces", {
+  ciphertext <- "O M G"
   expected <- "lnt"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode mindblowingly", {
-  plaintext <- "mindblowingly"
+test_that("encode mindblowingly", {
+  ciphertext <- "mindblowingly"
   expected <- "nrmwy oldrm tob"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode numbers", {
-  plaintext <- "Testing,1 2 3, testing."
+test_that("encode numbers", {
+  ciphertext <- "Testing,1 2 3, testing."
   expected <- "gvhgr mt123 gvhgr mt"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode deep thought", {
-  plaintext <- "Truth is fiction."
+test_that("encode deep thought", {
+  ciphertext <- "Truth is fiction."
   expected <- "gifgs rhurx grlm"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Encode all the letters", {
-  plaintext <- "The quick brown fox jumps over the lazy dog."
+test_that("encode all the letters", {
+  ciphertext <- "The quick brown fox jumps over the lazy dog."
   expected <- "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
-  expect_equal(encode(plaintext), expected)
+  expect_equal(encode(ciphertext), expected)
 })
 
-test_that("Decode exercism", {
+
+# decode
+
+test_that("decode exercism", {
   ciphertext <- "vcvix rhn"
   expected <- "exercism"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode a sentence", {
+test_that("decode a sentence", {
   ciphertext <- "zmlyh gzxov rhlug vmzhg vkkrm thglm v"
   expected <- "anobstacleisoftenasteppingstone"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode numbers", {
+test_that("decode numbers", {
   ciphertext <- "gvhgr mt123 gvhgr mt"
   expected <- "testing123testing"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode all the letters", {
+test_that("decode all the letters", {
   ciphertext <- "gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt"
   expected <- "thequickbrownfoxjumpsoverthelazydog"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode with too many spaces", {
+test_that("decode with too many spaces", {
   ciphertext <- "vc vix    r hn"
   expected <- "exercism"
   expect_equal(decode(ciphertext), expected)
 })
 
-test_that("Decode with no spaces", {
+test_that("decode with no spaces", {
   ciphertext <- "zmlyhgzxovrhlugvmzhgvkkrmthglmv"
   expected <- "anobstacleisoftenasteppingstone"
   expect_equal(decode(ciphertext), expected)
