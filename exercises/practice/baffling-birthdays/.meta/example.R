cd074bc5b2c1b3c790e263669a1a400acfea42ed
Colin Leach <colinleach@pm.me>
2026-03-26 17:14:05 -0700

    [Practice exercise] Baffling Birthdays (#496)
    * [Practice exercise] Baffling Birthdays
    
    * changed library imports

---
+++ exercises/practice/baffling-birthdays/.meta/example.R
@@ -0,0 +1,19 @@
+library(lubridate)
+library(dplyr)
+
+shared_birthday <- function(birthdates) {
+  birthdays <- birthdates |> str_sub(6)
+  length(birthdays) > length(unique(birthdays))
+}
+
+random_birthdates <- function(groupsize) {
+  randyear <- sample(1900:year(today()), groupsize, replace = TRUE)
+  no_leaps <- if_else(leap_year(randyear), randyear - 1, randyear)
+  make_date(year = no_leaps) + days(sample(0:364, groupsize, replace = TRUE))
+}
+
+estimate_probability_of_shared_birthday <- function(groupsize) {
+  reps <- 500
+  runs <- replicate(reps, shared_birthday(random_birthdates(groupsize)))
+  sum(runs) / reps
+}
---
+++ exercises/practice/baffling-birthdays/baffling-birthdays.R
@@ -0,0 +1,11 @@
+shared_birthday <- function(birthdates) {
+
+}
+
+random_birthdates <- function(groupsize) {
+
+}
+
+estimate_probability_of_shared_birthday <- function(groupsize) {
+
+}
