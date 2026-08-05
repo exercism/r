4cb228ed330206c333c59930654be6ee5c30fb40
Colin Leach <colinleach@pm.me>
2026-08-05 15:55:24 -0700

    [Update exercise] Crypto Square (#539)
    * [Update exercise] Crypto Suare
    
    * linter fix
    
    * changed library import

--- exercises/practice/crypto-square/test_crypto-square.R
+++ exercises/practice/crypto-square/test_crypto-square.R
@@ -1,68 +1,31 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/crypto-square/canonical-data.json
+# File last updated on 2026-08-05
+
 source("./crypto-square.R")
 library(testthat)
-
-test_that("Lowercase", {
-  expect_equal(normalized_plaintext("Hello"), "hello")
-})
-
-test_that("Remove spaces", {
-  expect_equal(normalized_plaintext("Hi there"), "hithere")
-})
-
-test_that("Remove punctuation", {
-  expect_equal(normalized_plaintext("@1, 2%, 3 Go!"), "123go")
-})
-
-test_that("empty plaintext results in an empty rectangle", {
-  expect_equal(plaintext_segments(""), "")
-})
-
-test_that("4 character plaintext results in an 2x2 rectangle", {
-  expect_equal(plaintext_segments("Ab Cd"), c("ab", "cd"))
-})
-
-test_that("9 character plaintext results in an 3x3 rectangle", {
-  expect_equal(plaintext_segments("This is fun!"), c("thi", "sis", "fun"))
-})
-
-test_that("54 character plaintext results in an 8x7 rectangle", {
-  expect_equal(
-    plaintext_segments(
-      "If man was meant to stay on the ground, god would have given us roots."
-    ),
-    c(
-      "ifmanwas",
-      "meanttos",
-      "tayonthe",
-      "groundgo",
-      "dwouldha",
-      "vegivenu",
-      "sroots"
-    )
-  )
-})
-
-test_that("empty plaintext results in an empty encode", {
-  expect_equal(encoded(""), "")
-})
-
-test_that("Non-empty plaintext results in the combined plaintext segments", {
-  expect_equal(
-    encoded(
-      "If man was meant to stay on the ground, god would have given us roots."
-    ),
-    "imtgdvsfearwermayoogoanouuiontnnlvtwttddesaohghnsseoau"
-  )
-})
 
 test_that("empty plaintext results in an empty ciphertext", {
   expect_equal(ciphertext(""), "")
 })
-
+test_that("normalization results in empty plaintext", {
+  expect_equal(ciphertext("... --- ..."), "")
+})
+test_that("Lowercase", {
+  expect_equal(ciphertext("A"), "a")
+})
+test_that("Remove spaces", {
+  expect_equal(ciphertext("  b "), "b")
+})
+test_that("Remove punctuation", {
+  expect_equal(ciphertext("@1,%!"), "1")
+})
 test_that("9 character plaintext results in 3 chunks of 3 characters", {
   expect_equal(ciphertext("This is fun!"), "tsf hiu isn")
 })
-
+test_that("8 character plaintext results in 3 chunks, the last one with a trailing space", {
+  expect_equal(ciphertext("Chill out."), "clu hlt io ")
+})
 test_that("54 character plaintext results in 8 chunks, the last two with trailing spaces", {
   expect_equal(
     ciphertext(
@@ -70,8 +33,4 @@ test_that("54 character plaintext results in 8 chunks, the last two with trailin
     ),
     "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau "
   )
-})
-
-test_that("normalization results in empty plaintext", {
-  expect_equal(ciphertext("... --- ..."), "")
 })
